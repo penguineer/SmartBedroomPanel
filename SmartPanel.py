e9717af3f797abef46bce608534f171f22944c73
Stefan Haun <tux@netz39.de>
2020-02-16 22:22:18 +0100

    Make alpha-channel a parameter for colors

--- SmartPanel.py
+++ SmartPanel.py
@@ -30,38 +30,38 @@ import paho.mqtt.client as mqtt
 
 class RMColor:
     @staticmethod
-    def get_rgba(name):
+    def get_rgba(name, alpha=1):
         # grey is fallback
-        c = (77, 77, 76, 256)
+        c = (77, 77, 76)
 
         if name == "off" or name == "black":
-            c = (0, 0, 0, 256)
+            c = (0, 0, 0)
 
         if name == "fresh" or name == "light blue":
-            c = (0, 132, 176, 256)
+            c = (0, 132, 176)
 
         if name == "hope" or name == "green":
-            c = (0, 163, 86, 256)
+            c = (0, 163, 86)
 
         if name == "glint" or name == "yellow":
-            c = (249, 176, 0, 256)
+            c = (249, 176, 0)
 
         if name == "beat" or name == "red":
-            c = (228, 5, 41, 256)
+            c = (228, 5, 41)
 
         if name == "tenacity" or name == "lilac":
-            c = (68, 53, 126, 256)
+            c = (68, 53, 126)
 
         if name == "base" or name == "dark blue":
-            c = (24, 56, 107, 256)
+            c = (24, 56, 107)
 
         # reboot (grey) is fallback from above
 
-        return list(map(lambda x: x/256, c))
+        return list(map(lambda x: x/256, c)) + [alpha]
 
     @staticmethod
-    def get_color(name):
-        c = RMColor.get_rgba(name)
+    def get_color(name, alpha=1):
+        c = RMColor.get_rgba(name, alpha)
         return Color(c[0], c[1], c[2], c[3], mode='rgba')
 
 
